0e053f2e80b5d6c517550ea2405606f258897aaa
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-10-18 13:24:43 +0300

    update model sqlalchmey

--- server/app/model.py
+++ server/app/model.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from datetime import datetime
@@ -6,18 +8,27 @@ from datetime import datetime
 class Role(Base):
     __tablename__ = "roles"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.now())
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
 
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return f"Role(id={self.id!r}, name={self.name!r})"
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime, default=datetime.now())
-    role_id = Column(Integer, ForeignKey("roles.id"), default=1)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    email: Mapped[str] = mapped_column(String, unique=True)
+    password: Mapped[str] = mapped_column(String)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, username={self.username!r}, email={self.email!r})"
+
